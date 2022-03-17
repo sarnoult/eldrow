2b593ff9e113e6a4011ebac6a13465f2a781cd95
sarnoult <sophie.arnoult@posteo.net>
2022-03-17 17:37:23 +0100

    add unit tests

--- eldrow.py
+++ eldrow.py
@@ -1,4 +1,5 @@
 import argparse
+from typing import List
 
 from colorama import init, Style, Back
 from urllib.request import urlopen
@@ -9,19 +10,23 @@ init()
 VOCAB_URL = "https://raw.githubusercontent.com/jason-chao/wordle-solver/main/english_words_original_wordle.txt"
 VOCABULARY = [line.strip() for line in urlopen(VOCAB_URL)]
 
+WORD_LENGTH = 5
 
-def pick_word():
+
+def pick_word() -> str:
+    """Selects a word from the vocabulary"""
     word = choice(VOCABULARY).decode("utf-8")
     print("Let's play wordle!")
     print(f"Correct word (for testing): {word}")
     return word
 
 
-def read_guess(attempt_nb):
+def read_guess(attempt_nb: int) -> str:
+    """Prompts the user for a guess"""
     print(f"Enter your next guess:")
     while True:
         guess = input(f"[{attempt_nb}] ")
-        if len(guess) != 5:
+        if len(guess) != WORD_LENGTH:
             print("Please enter a 5-letter word")
         elif guess.encode("utf-8") not in VOCABULARY:
             print("Unknown word, try again")
@@ -30,25 +35,36 @@ def read_guess(attempt_nb):
     return guess
 
 
-def check(guess, word):
-    output = []
-    for i in range(5):
-        if guess[i] == word[i]:
-            output.append(Back.GREEN)
-        elif guess[i] in word:
-            output.append(Back.YELLOW)
-        output.append(guess[i])
-        output.append(Style.RESET_ALL)
-    return output
+def color_correct(char: str) -> List[str]:
+    return "".join([Back.GREEN, char, Style.RESET_ALL])
 
 
-def play(max_nb_attempts):
+def color_inword(char: str) -> List[str]:
+    return "".join([Back.YELLOW, char, Style.RESET_ALL])
+
+
+def validate_char(i: int, guess: str, solution: str) -> str:
+    if guess[i] == solution[i]:
+        return color_correct(guess[i])
+    elif guess[i] in solution:
+        return color_inword(guess[i])
+    else:
+        return guess[i]
+
+
+def validate(guess: str, solution: str) -> str:
+    """Validates the user guess against the solution, coloring correct characters"""
+    return "".join([validate_char(i, guess, solution) for i in range(WORD_LENGTH)])
+
+
+def play(max_nb_attempts: int):
+    """Plays wordle until user finds solution or number of attempts is exceeded"""
     word = pick_word()
     found_solution = False
     for attempt in range(1, max_nb_attempts + 1):
         guess = read_guess(attempt)
-        output = check(guess, word)
-        print("".join(output))
+        output = validate(guess, word)
+        print(output)
         if word == guess:
             print(f"Congrats! You needed {attempt} attempts")
             found_solution = True
@@ -57,8 +73,8 @@ def play(max_nb_attempts):
 
 
 if __name__ == '__main__':
-    # parser = argparse.ArgumentParser(description=__doc__)
-    # parser.add_argument("max_nb_attempts", type=int, help="Maximum number of attempts")
-    # parser.add_argument("word_length", type=int, help="Word length")
-    # args = parser.parse_args()
-    play(6)
+
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("max_nb_attempts", type=int, default=10, help="Maximum number of attempts")
+    args = parser.parse_args()
+    play(args.max_nb_attempts)
--- test_eldrow.py
+++ test_eldrow.py
@@ -1,2 +1,14 @@
-def test_eldrow():
-    pass
+from eldrow import pick_word, color_correct, color_inword, validate, WORD_LENGTH
+
+
+def test_pick_word():
+    word = pick_word()
+    assert len(word) == WORD_LENGTH
+
+
+def test_validate():
+    word = "hello"
+    guess = "hopla"
+    expect = "".join([color_correct('h'), color_inword('o'), 'p', color_correct('l'), 'a'])
+    assert validate(guess, word) == expect
+
